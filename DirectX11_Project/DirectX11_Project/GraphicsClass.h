8b2cc431517f1d86f0b1702dea36d809010d8008
Taewoo Kim <kimsin3003@naver.com>
2016-05-07 23:20:47 +0900

    Triangle is on display.

--- DirectX11_Project/DirectX11_Project/GraphicsClass.h
+++ DirectX11_Project/DirectX11_Project/GraphicsClass.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <windows.h>
 #include "D3DClass.h"
+#include "CameraClass.h"
+#include "ModelClass.h"
+#include "ColorShaderClass.h"
 
 
 const bool FULL_SCREEN = false;
@@ -20,4 +23,7 @@ private:
 
 private:
 	D3DClass* m_Direct3D;
+	CameraClass* m_Camera;
+	ModelClass* m_Model;
+	ColorShaderClass* m_ColorShader;
 };
